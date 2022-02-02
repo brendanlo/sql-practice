5ddddd89e7a9906cd8ed910346919bdaa09ed31a
brendan <lobrendan@gmail.com>
2022-02-01 16:09:16 -0800

    initial commit, wrote outer_space

---
+++ outer_space.sql
@@ -0,0 +1,32 @@
+# DDL 
+
+PSQL
+
+CREATE DATABASE outer_space;
+\c outer_space;
+
+CREATE TABLE moons (
+    moon varchar(100) PRIMARY KEY,
+    planet varchar(100) NOT NULL REFERENCES planets
+);
+
+CREATE TABLE stars (
+    star varchar(100) PRIMARY KEY,
+    star_temp int
+);
+
+CREATE TABLE planets (
+    planet varchar(100) PRIMARY KEY,
+    orbit_period float,
+    star varchar(100) REFERENCES stars 
+    -- should this be not null?
+);
+
+-- querying data
+SELECT planets.planet, stars.star, count(moon) as moon_count
+FROM planets
+    JOIN stars 
+        ON planets.star = stars.star
+    JOIN moons
+        ON moons.planet = planets.planet
+GROUP BY planets.planet, stars.star
